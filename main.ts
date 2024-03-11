69639b5ba0837a12da9f9ed9b763964bafd13ef0
Sami Ullah <153662652+samikhan1622@users.noreply.github.com>
2024-03-11 13:01:31 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,56 @@
+          //functions
+
+// function greet() {
+//     console.log("hello sami");
+// }
+// function sum() {
+//     console.log(2 + 2);
+// }
+// greet();
+// greet();
+// sum();
+
+
+
+// function greet(name: string) {
+// console.log("hello", name);
+// }
+
+// greet("hamza");
+// greet("ali")
+
+
+//       // (parameters function)
+
+// function sum(num1: number, num2: number){
+//     console.log(num1 + num2);
+
+// }
+
+// sum (5, 10)
+
+
+
+// function sum(num1: number, num2: number) {
+//     // console.log(num1 + num2);
+//     return num1 + num2;
+// }
+// let ans = sum(5, 10)
+
+// function multiply(num1: number){
+//     console.log(num1 * 2);
+    
+// }
+
+// multiply(ans)
+
+
+
+//function simple..
+
+// function NumOfCzn (sami:number, aqif:number, saqib:number){
+//     return sami + aqif + saqib
+// }
+// console.log(NumOfCzn (50, 30, 10));
+
+
